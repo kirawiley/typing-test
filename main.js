37ebf9883b5d931723896aef4824472f11daa816
Kira Wiley <kaysimone5@gmail.com>
2017-03-10 15:50:11 -0800

    Sentence is visible. #1

---
+++ main.js
@@ -0,0 +1,6 @@
+var mySentence = 'The dog chased a rabbit through our yard.';
+
+var $mySentence = document.createElement('h1');
+$mySentence.textContent = mySentence;
+$mySentence.id = 'sentence';
+document.body.appendChild($mySentence)
