69036d61250922906b2949cd2e94812e0110579d
Kira Wiley <kaysimone5@gmail.com>
2017-03-13 13:20:16 -0700

    test

--- main.js
+++ main.js
@@ -4,5 +4,22 @@ for (var i = 0; i < mySentence.length; i++) {
  var $mySentence = document.createElement('span')
  $mySentence.textContent = mySentence[i]
  document.body.appendChild($mySentence)
- $mySentence.id = 'sentence'
 }
+
+var $firstLetter = document.querySelector('span')
+$firstLetter.classList.add('current')
+
+
+var $current = document.querySelector('.current')
+var $currentCharacter = $current.textContent
+
+document.addEventListener('keydown', function (event) {
+  if (event.key === $currentCharacter) {
+    $current.classList.remove('current')
+    $current.nextSibling.classList.add('current')
+    $current.classList.add('correct')
+  }
+  else {
+    $current.classList.add('wrong')
+  }
+})
