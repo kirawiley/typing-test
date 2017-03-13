d0c150a19baf9ec53ec8cffb598a2908ed943812
Kira Wiley <kaysimone5@gmail.com>
2017-03-13 14:16:37 -0700

    User can view prompt. #2

--- main.js
+++ main.js
@@ -8,18 +8,3 @@ for (var i = 0; i < mySentence.length; i++) {
 
 var $firstLetter = document.querySelector('span')
 $firstLetter.classList.add('current')
-
-
-var $current = document.querySelector('.current')
-var $currentCharacter = $current.textContent
-
-document.addEventListener('keydown', function (event) {
-  if (event.key === $currentCharacter) {
-    $current.classList.remove('current')
-    $current.nextSibling.classList.add('current')
-    $current.classList.add('correct')
-  }
-  else {
-    $current.classList.add('wrong')
-  }
-})
