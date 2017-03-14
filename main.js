bbcc2303028261de4114ec6b96cfddd75cf2d01a
Kira Wiley <kaysimone5@gmail.com>
2017-03-14 09:31:13 -0700

    User recieves score. #4

--- main.js
+++ main.js
@@ -1,34 +1,52 @@
+//Global variables
 var mySentence = 'The dog chased a rabbit across our yard.'
+var wrongAnswer = 0
+var correctKeys = 1
 
+//Separates letters with spans
 for (var i = 0; i < mySentence.length; i++) {
  var $mySentence = document.createElement('span')
  $mySentence.textContent = mySentence[i]
  document.body.appendChild($mySentence)
 }
 
+//Underlines current letter
 var $firstLetter = document.querySelector('span')
 $firstLetter.classList.add('current')
 
-
+//Changes letter based on user's key press
 document.addEventListener('keydown', function (event) {
-  var $current = document.querySelector('.current')
-  var $currentCharacter = $current.textContent
-  
-  if (event.key === $currentCharacter) {
-    $current.classList.remove('current')
+  if (correctKeys === mySentence.length) {
+    showScore()}
 
-  if ($current.nextSibling.nodeType === 1){
-    $current.nextSibling.classList.add('current')
-    $current.classList.add('correct')
-    }
-  else {
-    var errorNumber = document.querySelectorAll('.wrong').length
-    var $score = document.createTextNode('You made' + ' ' + errorNumber + ' ' + 'mistakes.')
-    return $score
-    }
-  }
+    var $current = document.querySelector('.current')
+    var $currentCharacter = $current.textContent
 
-  else {
-    $current.classList.add('wrong')
-  }
+//If answer is correct...
+    if (event.key === $currentCharacter) {
+        correctKeys = correctKeys + 1
+        $current.classList.remove('current')
+        $current.nextSibling.classList.add('current')
+        $current.classList.add('correct')
+        }
+
+//If answer is wrong...
+    else {
+      $current.classList.add('wrong')
+      wrongAnswer = wrongAnswer + 1}
 })
+
+//Shows the user their score
+function showScore () {
+  var $score = document.createElement('div')
+  if (wrongAnswer === 1) {
+    $score.textContent = 'You made' + ' ' + wrongAnswer + ' ' + 'mistake.'
+  }
+  else if (wrongAnswer === 0) {
+    $score.textContent = 'You made no mistakes. Great job!'
+  }
+  else {
+    $score.textContent = 'You made' + ' ' + wrongAnswer + ' ' + 'mistakes.'
+  }
+  document.body.appendChild($score)
+}
